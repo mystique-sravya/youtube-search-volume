c0cbc6084e4692d982b202cb7f8ebd80ccb4a58d
mystique_sravya <sravyanalanagula2003@gmail.com>
2023-08-09 22:05:50 +0530

    added project files

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('searchButton').addEventListener('click', async () => {
+    const keyword = document.getElementById('keywordInput').value;
+    if (keyword.trim() === '') {
+        alert('Please enter a keyword');
+        return;
+    }
+    
+    const youtubeSearchVolume = await fetchYouTubeSearchVolume(keyword);
+
+    const resultContainer = document.getElementById('resultContainer');
+    resultContainer.innerHTML = `Keyword: ${keyword}<br>YouTube Search Volume: ${youtubeSearchVolume}`;
+});
+
+async function fetchYouTubeSearchVolume(keyword) {
+    const apiKey = 'AIzaSyAVCeqo4RcSF7MC93165x83z3L4gm0iyoo';
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(keyword)}&key=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const totalResults = data.pageInfo.totalResults;
+        return totalResults;
+    } catch (error) {
+        console.error('Error fetching YouTube data:', error);
+        return 'N/A';
+    }
+}
+
+
